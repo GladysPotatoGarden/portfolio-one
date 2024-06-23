f616632f0fd10dcece3138e8fe36413d11137cb0
Quincy Z <digitalkotaz@gmail.com>
2024-06-23 17:05:08 -0400

    footer
    created footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,13 @@ import './Footer.css'
 function Footer() {
   return (
     <section>
-        <h1>text</h1>
+        <div className="footer">
+          <a href="#" className="footer-link">About</a>
+          <a href="#" className="footer-link">Projects</a>
+          <a href="#" className="footer-link">Resume</a>
+          <a href="#" className="footer-link">Contact</a>
+          <h1 className="footer-text">Copyright© Quincy Zientek</h1>
+        </div>
     </section>
   );
 }
