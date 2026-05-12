c142e3f8625634f2062d5b872e3185c5da7e101a
GladysPotatoGarden <282367955+stellystellybot-ops@users.noreply.github.com>
2026-05-12 17:01:50 -0400

    Revert "Add DV WRAP project to top of Projects section"
    This reverts commit bad5f7e00670d83e9889f3dd769d8f32650f62e8.

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,5 +1,4 @@
 import "./Projects.css";
-import DVWrapImg from "./assets/DVWrap.png";
 import Quazar from "./assets/Quazar.png";
 import SirFizzaloImg from "./assets/SirFizzalo.png";
 import SirFizzaloHarmonicExciterImg from "./assets/SirFizzaloHarmonicExciter.png";
@@ -15,19 +14,6 @@ function Projects() {
       <Reveal minStage={7}>
         <div className="project-item">
           <figure className="project-figure">
-            <img
-              className="project-image"
-              src={DVWrapImg}
-              alt="DV WRAP"
-            />
-            <figcaption className="project-label">DV WRAP</figcaption>
-          </figure>
-        </div>
-      </Reveal>
-
-      <Reveal minStage={8}>
-        <div className="project-item">
-          <figure className="project-figure">
             <a
               href="https://admirable-cupcake-4b5cdf.netlify.app/"
               target="_blank"
@@ -40,7 +26,7 @@ function Projects() {
         </div>
       </Reveal>
 
-      <Reveal minStage={9}>
+      <Reveal minStage={8}>
         <div className="project-item">
           <figure className="project-figure">
             <img
@@ -53,7 +39,7 @@ function Projects() {
         </div>
       </Reveal>
 
-      <Reveal minStage={10}>
+      <Reveal minStage={9}>
         <div className="project-item">
           <figure className="project-figure">
             <img
@@ -68,7 +54,7 @@ function Projects() {
         </div>
       </Reveal>
 
-      <Reveal minStage={11}>
+      <Reveal minStage={10}>
         <div className="project-item">
           <figure className="project-figure">
             <img
